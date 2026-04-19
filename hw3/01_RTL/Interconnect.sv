753409e38abb710f8d751c75ceb0f1e0107a9838
bunbun221008 <leeoo1008a@gmail.com>
2026-04-19 14:32:14 +0800

    interconnect and ahb done

--- hw3/01_RTL/Interconnect.sv
+++ hw3/01_RTL/Interconnect.sv
@@ -2,8 +2,162 @@ module Interconnect #(
     parameter   DATA_WIDTH = 16
 ) (
     input   logic   i_clk,
-    input   logic   i_rst_n
-);
+    input   logic   i_rst_n,
+    input   logic   irq,
 
-    
+    // tb interface
+    input   logic   [  13:0] tb_m_HADDR,
+    input   logic            tb_m_HWRITE,
+    input   logic   [   2:0] tb_m_HSIZE,
+    input   logic   [   2:0] tb_m_HBURST,
+    input   logic   [   1:0] tb_m_HTRANS,
+    input   logic   [1023:0] tb_m_HWDATA,
+    output  logic            tb_m_HREADY,
+    output  logic            tb_m_HRESP,
+    output  logic   [1023:0] tb_m_HRDATA,
+
+    // SIMD core interface
+    input   logic   [  13:0] simd_m_HADDR,
+    input   logic            simd_m_HWRITE,
+    input   logic   [   2:0] simd_m_HSIZE,
+    input   logic   [   2:0] simd_m_HBURST,
+    input   logic   [   1:0] simd_m_HTRANS,
+    input   logic   [1023:0] simd_m_HWDATA,
+    output  logic            simd_m_HREADY,
+    output  logic            simd_m_HRESP,
+    output  logic   [1023:0] simd_m_HRDATA,
+
+    output   logic   [  13:0] simd_s_HADDR,
+    output   logic            simd_s_HWRITE,
+    output   logic   [   2:0] simd_s_HSIZE,
+    output   logic   [   2:0] simd_s_HBURST,
+    output   logic   [   1:0] simd_s_HTRANS,
+    output   logic   [1023:0] simd_s_HWDATA,
+    input    logic            simd_s_HREADY,
+    input    logic            simd_s_HRESP,
+    input    logic   [1023:0] simd_s_HRDATA,
+
+    // GEMM core interface
+    input   logic   [  13:0] gemm_m_HADDR,
+    input   logic            gemm_m_HWRITE,
+    input   logic   [   2:0] gemm_m_HSIZE,
+    input   logic   [   2:0] gemm_m_HBURST,
+    input   logic   [   1:0] gemm_m_HTRANS,
+    input   logic   [1023:0] gemm_m_HWDATA,
+    output  logic            gemm_m_HREADY,
+    output  logic            gemm_m_HRESP,
+    output  logic   [1023:0] gemm_m_HRDATA,
+
+    output   logic   [  13:0] gemm_s_HADDR,
+    output   logic            gemm_s_HWRITE,
+    output   logic   [   2:0] gemm_s_HSIZE,
+    output   logic   [   2:0] gemm_s_HBURST,
+    output   logic   [   1:0] gemm_s_HTRANS,
+    output   logic   [1023:0] gemm_s_HWDATA,
+    input    logic            gemm_s_HREADY,
+    input    logic            gemm_s_HRESP,
+    input    logic   [1023:0] gemm_s_HRDATA,
+
+    // Shared Memory interface
+    output   logic   [  13:0] mem_s_HADDR,
+    output   logic            mem_s_HWRITE,
+    output   logic   [   2:0] mem_s_HSIZE,
+    output   logic   [   2:0] mem_s_HBURST,
+    output   logic   [   1:0] mem_s_HTRANS,
+    output   logic   [1023:0] mem_s_HWDATA,
+    input    logic            mem_s_HREADY,
+    input    logic            mem_s_HRESP,
+    input    logic   [1023:0] mem_s_HRDATA,
+);
+    //////////////////////////parameter /////////////////////////////////////
+    typedef enum logic [1:0] { 
+        S_MEM,
+        S_SIMD,
+        S_GEMM,
+    } state;
+    ////////////////////////////reg and wire declarations//////////////////////////
+    state state_w, state_r;
+
+
+    /////////////////////////////assignments/////////////////////////////
+    // simd
+    assign simd_s_HADDR = tb_m_HADDR;
+    assign simd_s_HSIZE = tb_m_HSIZE;
+    assign simd_s_HBURST = tb_m_HBURST;
+    assign simd_s_HTRANS = tb_m_HTRANS;
+    assign simd_s_HWDATA = tb_m_HWDATA;
+    assign simd_s_HWRITE = tb_m_HWRITE;
+
+    assign simd_m_HREADY = mem_s_HREADY;
+    assign simd_m_HRESP = mem_s_HRESP;
+    assign simd_m_HRDATA = mem_s_HRDATA;
+
+    // gemm
+    assign gemm_s_HADDR = tb_m_HADDR;
+    assign gemm_s_HSIZE = tb_m_HSIZE;
+    assign gemm_s_HBURST = tb_m_HBURST;
+    assign gemm_s_HTRANS = tb_m_HTRANS;
+    assign gemm_s_HWDATA = tb_m_HWDATA;
+    assign gemm_s_HWRITE = tb_m_HWRITE;
+
+    assign gemm_m_HREADY = mem_s_HREADY;
+    assign gemm_m_HRESP = mem_s_HRESP;
+    assign gemm_m_HRDATA = mem_s_HRDATA;
+
+
+    // shared memory
+    always_comb begin
+        state_w = state_r;
+        case (state_r)
+            S_MEM: begin
+                if(tb_m_HWRITE && tb_m_HTRANS[1] && tb_m_HWDATA[15:0] == 16'd1)
+                    if(tb_m_HADDR == 14'h2000) state_w = S_SIMD;
+                    else if(tb_m_HADDR == 14'h2400) state_w = S_GEMM;
+
+                // tb connect to shared memory
+                mem_s_HADDR = tb_m_HADDR;
+                mem_s_HSIZE = tb_m_HSIZE;
+                mem_s_HBURST = tb_m_HBURST;
+                mem_s_HTRANS = tb_m_HTRANS;
+                mem_s_HWDATA = tb_m_HWDATA;
+                mem_s_HWRITE = tb_m_HWRITE;
+
+                tb_m_HREADY = mem_s_HREADY;
+                tb_m_HRESP = mem_s_HRESP;
+                tb_m_HRDATA = mem_s_HRDATA;
+
+            end 
+            S_SIMD: begin
+                if(irq) state_w = S_MEM;
+
+                // SIMD core connect to shared memory
+                mem_s_HADDR = simd_m_HADDR;
+                mem_s_HSIZE = simd_m_HSIZE;
+                mem_s_HBURST = simd_m_HBURST;
+                mem_s_HTRANS = simd_m_HTRANS;
+                mem_s_HWDATA = simd_m_HWDATA;
+                mem_s_HWRITE = simd_m_HWRITE;
+
+                tb_m_HREADY = simd_s_HREADY;
+                tb_m_HRESP = simd_s_HRESP;
+                tb_m_HRDATA = simd_s_HRDATA;
+            end 
+            S_GEMM: begin
+                if(irq) state_w = S_MEM;
+
+                // GEMM core connect to shared memory
+                mem_s_HADDR = gemm_m_HADDR;
+                mem_s_HSIZE = gemm_m_HSIZE;
+                mem_s_HBURST = gemm_m_HBURST;
+                mem_s_HTRANS = gemm_m_HTRANS;
+                mem_s_HWDATA = gemm_m_HWDATA;
+                mem_s_HWRITE = gemm_m_HWRITE;
+
+                tb_m_HREADY = gemm_s_HREADY;
+                tb_m_HRESP = gemm_s_HRESP;
+                tb_m_HRDATA = gemm_s_HRDATA;
+            end 
+            default: 
+        endcase
+    end
 endmodule
---
+++ hw3/01_RTL/ahb.sv
@@ -0,0 +1,91 @@
+module ahb_slave #(
+    parameter   DATA_WIDTH = 16
+) (
+    input   logic   i_clk,
+    input   logic   i_rst_n,
+
+    // ahb signals
+    input   logic   [  13:0] HADDR,
+    input   logic            HWRITE,
+    input   logic   [   2:0] HSIZE,
+    input   logic   [   2:0] HBURST,
+    input   logic   [   1:0] HTRANS,
+    input   logic   [1023:0] HWDATA,
+    output  logic            HREADY,
+    output  logic            HRESP,
+    output  logic   [1023:0] HRDATA,
+    // to core
+    output  logic   [  13:0] ahb_addr,
+    output  logic   [   2:0] ahb_size,
+    output  logic   [1023:0] ahb_wdata,
+    output  logic            ahb_write,
+    input   logic   [1023:0] ahb_rdata,
+    output  logic            ahb_read,
+    input   logic            ahb_ready,
+);
+    logic [1:0] error_w, error_r;
+    logic ready;
+
+    assign ahb_addr = HADDR;
+    assign ahb_size = HSIZE;
+    assign ahb_wdata = HWDATA;
+    assign ahb_write = HWRITE && HTRANS[1] && !(error_r || error_w);
+    assign ahb_read = ~HWRITE && HTRANS[1] && !(error_r || error_w);
+    assign HREADY = ready;
+    assign HRESP = |error_r;
+    assign HRDATA = ahb_rdata;
+
+    always_comb begin;
+        error_w = {error_r[0],1'b0};
+        if((HADDR>=14'h1800 && HADDR<=14'h1FFF) || (HADDR>=14'h2C00 && HADDR<=14'h3FFF))
+            error_w = 2'b01;
+
+        if(error_r[0]) ready = 1'b0;
+        else if(error_r[1]) ready = 1'b1;
+        else ready = ahb_ready;
+    end 
+
+    always_ff @( posedge i_clk or negedge i_rst_n ) begin
+        if ( !i_rst_n ) begin
+            error_r <= 2'b00;
+        end
+        else begin
+            error_r <= error_w;
+        end
+    end
+endmodule
+
+module ahb_master #(
+    parameter   DATA_WIDTH = 16
+) (
+    input   logic   i_clk,
+    input   logic   i_rst_n,
+
+    output  logic   [  13:0] HADDR,
+    output  logic            HWRITE,
+    output  logic   [   2:0] HSIZE,
+    output  logic   [   2:0] HBURST,
+    output  logic   [   1:0] HTRANS,
+    output  logic   [1023:0] HWDATA,
+    input   logic            HREADY,
+    input   logic            HRESP,
+    input   logic   [1023:0] HRDATA
+
+    // to core
+    input  logic   [  13:0] ahb_addr,
+    input  logic   [   2:0] ahb_size,
+    input  logic   [1023:0] ahb_wdata,
+    input  logic            ahb_write,
+    output   logic   [1023:0] ahb_rdata,
+    input  logic            ahb_read,
+    output   logic            ahb_ready,
+);
+    assign HADDR = ahb_addr;
+    assign HSIZE = ahb_size;
+    assign HBURST = 3'b000;
+    assign HWDATA = ahb_wdata;
+    assign HWRITE = ahb_write;
+    assign HTRANS = (ahb_write || ahb_read) ? 2'b10 : 2'b00;
+    assign ahb_rdata = HRDATA;
+    assign ahb_ready = HREADY && !HRESP;
+endmodule
